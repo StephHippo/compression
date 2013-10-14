9df444884fb8e86733cba14f6df0a563c3079ce9
stephhippo <stephanie.hippo@gmail.com>
2013-10-13 23:47:00 -0400

    added 3 test cases from pseudocode

--- spec/Segmentator_spec.rb
+++ spec/Segmentator_spec.rb
@@ -1,12 +1,20 @@
 require './Segmentator.rb'
 
 describe Segmentator do
+  describe "build_segments" do
+    it "streams characters of a file and compresses segments of size k" do
+      @seg = Segmentator.new(3, "testcases/camesawleft.txt")
+      @seg.stream_characters()
+    end
 
-  before :each do
+    it "streams characters of a file and compresses segments of size k" do
+      @seg = Segmentator.new(8, "testcases/mango.txt")
+      @seg.stream_characters()
+    end
 
+    it "streams characters of a file and compresses segments of size k" do
+      @seg = Segmentator.new(2, "testcases/empty.txt")
+      @seg.stream_characters()
+    end
   end
-
-
-
-
 end
