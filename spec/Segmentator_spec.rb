e4b4cf8f13b82926605caecd2a75cf1738f1ae95
stephhippo <stephanie.hippo@gmail.com>
2013-10-27 12:50:09 -0400

    Added build_segment tests

--- spec/Segmentator_spec.rb
+++ spec/Segmentator_spec.rb
@@ -6,12 +6,14 @@ require './spec/spec_helper.rb'
 describe Segmentator do
   describe "stream_characters" do
 
+    #Structured Basis: each each_char loop
     #Good Data
     it "streams characters of a file and compresses segments of size k" do
       @seg = Segmentator.new(3, "testcases/camesawleft.txt")
       @seg.stream_characters()
     end
 
+    #Structured Basis: Nothing to stream so skips each_char loop
     it "streams characters of a file and compresses segments of size k" do
       @seg = Segmentator.new(8, "testcases/mango.txt")
       @seg.stream_characters()
@@ -26,41 +28,129 @@ describe Segmentator do
     #Structured Basis, Bad Data: nonexistent file
     it "streams characters of a new file and compresses segments of size k" do
       @seg = Segmentator.new(3, "testcases/nonexistentfile.txt")
-      @seg.stream_characters().should raise_error
+      lambda {@seg.stream_characters()}.should raise_error
     end
   end
 
   describe "build_segment" do
+    #1. Structured Basis: first if is true
+    #6: Data Flow: second if is false
+    #7. Good Data: c is a str of length 1, seg length is less than k
     context "c is punctuation, seg is length 0" do
       it "should calculate the compression of the c value and seg should be clear" do
         @segmentator = Segmentator.new(3, "testcases/mango.txt")
-        #TODO: DON'T DELETE THIS
-        #@segmentator.instance_eval{instance_variable_set(:@seg,)}
+        @segmentator.instance_eval{instance_variable_set(:@seg,'')}
         Segmentator.publicize(:build_segment) do
           @segmentator.build_segment('.')
         end
         seg = @segmentator.instance_eval{instance_variable_get(:@seg)}
         seg.eql? ''
+        #legend should contain c
+        legend = @segmentator.instance_eval{instance_variable_get(:@legend)}
+        legend.has_key? '.'
+        #update_compression_order should have the c's segnum first
+        co = @segmentator.instance_eval{instance_variable_get(:@compression_order)}
+        co[legend['.']]["next"].should be nil
+        co[legend['.']]["prev"].should be nil
       end
     end
 
-    #Structured Basis: first if is true
+    #1. Structured Basis, if is true
+    #5. Nested if is true
+    #7. Good Data: c is a str of length 1, seg length is less than k
+    context "c is punctuation, but seg already contains some characters" do
+       it "should calculate the compression for the seg that already contains chars and calculate the compression for c, then clear the seg" do
+         @segmentator = Segmentator.new(3, "testcases/mango.txt")
+         @segmentator.instance_eval{instance_variable_set(:@seg,'ab')}
+         Segmentator.publicize(:build_segment) do
+           @segmentator.build_segment('.')
+         end
+         seg = @segmentator.instance_eval{instance_variable_get(:@seg)}
+         seg.eql? ''
+         #legend should contain c and seg
+         legend = @segmentator.instance_eval{instance_variable_get(:@legend)}
+         legend.has_key? '.'
+         legend.has_key? 'ab'
+         #compression_order should have the c's segnum as first
+         #compression_order should have the seg's segnum as second
+         co = @segmentator.instance_eval{instance_variable_get(:@compression_order)}
+         co[legend['.']]["prev"].should be nil
+         co[legend['.']]["next"].should be legend['ab']
+         co[legend['ab']]["prev"].should be legend['.']
+         co[legend['ab']]["next"].should be nil
+       end
+    end
 
-    #Structured Basis: first if is false
+    #2. Structured Basis: first if is false
+    #3. Structured Basis: second if is true
+    #7. Good Data: c is a str of length 1
+    context "c is not a punctuation character, seg length is less than k" do
+      it "should append c to the seg" do
+        @segmentator = Segmentator.new(3, "testcases/mango.txt")
+        @segmentator.instance_eval{instance_variable_set(:@seg,'ab')}
+        Segmentator.publicize(:build_segment) do
+          @segmentator.build_segment('c')
+        end
+        seg = @segmentator.instance_eval{instance_variable_get(:@seg)}
+        seg.eql? 'abc'
+      end
+    end
 
-    #Structured Basis: second if is true
+    #2. Structured Basis: first if is false
+    #4. Structured Basis: second if is false
+    #7. Good Data: c is a str of length 1
+    context "c is not a punctuation character and seg length is not less than k" do
+      it "should calculate the compression of the full seg" do
+        @segmentator = Segmentator.new(3, "testcases/mango.txt")
+        @segmentator.instance_eval{instance_variable_set(:@seg,'abc')}
+        Segmentator.publicize(:build_segment) do
+          @segmentator.build_segment('d')
+        end
+        seg = @segmentator.instance_eval{instance_variable_get(:@seg)}
+        seg.eql? 'd'
+        #legend should contain c and seg
+        legend = @segmentator.instance_eval{instance_variable_get(:@legend)}
+        legend.has_key? 'abc'
+        #update_compression_order should have the c's segnum first
+        co = @segmentator.instance_eval{instance_variable_get(:@compression_order)}
+        co[legend['abc']]["next"].should be nil
+        co[legend['abc']]["prev"].should be nil
+      end
+    end
 
-    #Structured Basis: second if is false
+    #2. Structured Basis: first if is false
+    #4. Structured Basis: second if is false
+    #9. Equivalence Partition: seg length is 1 greater than k
+    context "seg length is greater than k" do
+      it "should raise an error" do
+        @segmentator = Segmentator.new(3, "testcases/mango.txt")
+        @segmentator.instance_eval{instance_variable_set(:@seg,'abcd')}
+        Segmentator.publicize(:build_segment) do
+          lambda {@segmentator.build_segment('e')}.should raise_error
+        end
+      end
+    end
 
-    #Data Flow: nested if is true
+    #8. Bad Data: c is not a str, c is an object
+    context "c is not a string" do
+      it "should raise an error" do
+        @segmentator = Segmentator.new(3, "testcases/mango.txt")
+        @segmentator.instance_eval{instance_variable_set(:@seg,'ab')}
+        Segmentator.publicize(:build_segment) do
+          lambda {@segmentator.build_segment(Object.new)}.should raise_error
+        end
+      end
+    end
 
-    #Data Flow: nested if is false
-
-    #Good Data: c is a str of length 1
-
-    #Bad Data: c is an object (wrong type)
-
-    #Equivalence Partitioning: seg length is greater than k
+    #1. Structured Basis: first if is true      X
+    #2. Structured Basis: first if is false     X
+    #3. Structured Basis: second if is true     X
+    #4. Structured Basis: second if is false    X
+    #5. Data Flow: nested if is true            X
+    #6. Data Flow: nested if is false           X
+    #7. Good Data: c is a str of length 1       X
+    #8. Bad Data: c is an object (wrong type)   X
+    #9. Equivalence Partitioning: seg length is greater than k    X
 
   end
 
