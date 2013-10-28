4840b2218e3ab3e479853d9b131c1981cffc2e01
stephhippo <stephanie.hippo@gmail.com>
2013-10-27 21:31:52 -0400

    Added before_each block to eliminate duplicated code

--- spec/Segmentator_spec.rb
+++ spec/Segmentator_spec.rb
@@ -4,31 +4,62 @@ require './spec/spec_helper.rb'
 
 
 describe Segmentator do
+  before_each do
+    @segmentator = Segmentator.new(3, "testcases/mango.txt")
+  end
+
   describe "stream_characters" do
 
-    #Structured Basis: each each_char loop
+    #asserts print_legend
+    context "calls print_legend" do
+      it "should print the legend after streaming all characters" do
+        #Check that both are asserted
+        Segmentator.publicize(:stream_characters) do
+          seg = @segmentator.instance_eval{instance_variable_get(:@seg)}
+          @segmentator.should_receive(:print_legend)
+          @segmentator.stream_characters()
+        end
+      end
+    end
+
+    #asserts update_compression_order
+    context "calls calculate_compression" do
+      it "should call calculate compression on the final seg" do    #Check that both are asserted
+        @segmentator.instance_eval{instance_variable_set(:@seg,'abc')}
+        Segmentator.publicize(:calculate_compression) do
+          seg = @segmentator.instance_eval{instance_variable_get(:@seg)}
+          @segmentator.should_receive(:update_compression_order)
+          @segmentator.calculate_compression(seg)
+        end
+      end
+    end
+
+    #Structured Basis: each_char loop
     #Good Data
     it "streams characters of a file and compresses segments of size k" do
-      @seg = Segmentator.new(3, "testcases/camesawleft.txt")
-      @seg.stream_characters()
+      @segmentator.stream_characters()
     end
 
     #Structured Basis: Nothing to stream so skips each_char loop
-    it "streams characters of a file and compresses segments of size k" do
-      @seg = Segmentator.new(8, "testcases/mango.txt")
-      @seg.stream_characters()
-    end
-
     #Bad Data: empty text
     it "streams characters of a file and compresses segments of size k" do
-      @seg = Segmentator.new(0, "testcases/empty.txt")
-      @seg.stream_characters()
+      @segmentator = Segmentator.new(0, "testcases/empty.txt")
+      @segmentator.stream_characters()
     end
 
-    #Structured Basis, Bad Data: nonexistent file
+    #Structured Basis: first if is true
+    #Bad Data: nonexistent file
     it "streams characters of a new file and compresses segments of size k" do
-      @seg = Segmentator.new(3, "testcases/nonexistentfile.txt")
-      lambda {@seg.stream_characters()}.should raise_error
+      @segmentator = Segmentator.new(3, "testcases/nonexistentfile.txt")
+      lambda {@segmentator.stream_characters()}.should raise_error
+    end
+
+    #Error guessing, inputting data out of order
+    context "user enters k and file name backwards" do
+      it "should raise an error" do
+        @seg = Segmentator.new("testcases/mango.txt", 3)
+        lambda {@seg.stream_characters()}.should raise_error
+      end
     end
   end
 
@@ -38,7 +69,6 @@ describe Segmentator do
     #7. Good Data: c is a str of length 1, seg length is less than k
     context "c is punctuation, seg is length 0" do
       it "should calculate the compression of the c value and seg should be clear" do
-        @segmentator = Segmentator.new(3, "testcases/mango.txt")
         @segmentator.instance_eval{instance_variable_set(:@seg,'')}
         Segmentator.publicize(:build_segment) do
           @segmentator.build_segment('.')
@@ -60,7 +90,6 @@ describe Segmentator do
     #7. Good Data: c is a str of length 1, seg length is less than k
     context "c is punctuation, but seg already contains some characters" do
        it "should calculate the compression for the seg that already contains chars and calculate the compression for c, then clear the seg" do
-         @segmentator = Segmentator.new(3, "testcases/mango.txt")
          @segmentator.instance_eval{instance_variable_set(:@seg,'ab')}
          Segmentator.publicize(:build_segment) do
            @segmentator.build_segment('.')
@@ -86,7 +115,6 @@ describe Segmentator do
     #7. Good Data: c is a str of length 1
     context "c is not a punctuation character, seg length is less than k" do
       it "should append c to the seg" do
-        @segmentator = Segmentator.new(3, "testcases/mango.txt")
         @segmentator.instance_eval{instance_variable_set(:@seg,'ab')}
         Segmentator.publicize(:build_segment) do
           @segmentator.build_segment('c')
@@ -101,7 +129,6 @@ describe Segmentator do
     #7. Good Data: c is a str of length 1
     context "c is not a punctuation character and seg length is not less than k" do
       it "should calculate the compression of the full seg" do
-        @segmentator = Segmentator.new(3, "testcases/mango.txt")
         @segmentator.instance_eval{instance_variable_set(:@seg,'abc')}
         Segmentator.publicize(:build_segment) do
           @segmentator.build_segment('d')
@@ -123,7 +150,6 @@ describe Segmentator do
     #9. Equivalence Partition: seg length is 1 greater than k
     context "seg length is greater than k" do
       it "should raise an error" do
-        @segmentator = Segmentator.new(3, "testcases/mango.txt")
         @segmentator.instance_eval{instance_variable_set(:@seg,'abcd')}
         Segmentator.publicize(:build_segment) do
           lambda {@segmentator.build_segment('e')}.should raise_error
@@ -134,7 +160,6 @@ describe Segmentator do
     #8. Bad Data: c is not a str, c is an object
     context "c is not a string" do
       it "should raise an error" do
-        @segmentator = Segmentator.new(3, "testcases/mango.txt")
         @segmentator.instance_eval{instance_variable_set(:@seg,'ab')}
         Segmentator.publicize(:build_segment) do
           lambda {@segmentator.build_segment(Object.new)}.should raise_error
@@ -155,12 +180,23 @@ describe Segmentator do
   end
 
   describe "calculate_compression" do
-    #TODO: Label
-    #
+    #asserts update_compression_order
     context "both methods are called" do
       it "should pass seg and segnum to add_to_legend and calculate_compression" do
         #Check that both are asserted
-        @segmentator = Segmentator.new(3, "testcases/mango.txt")
+        @segmentator.instance_eval{instance_variable_set(:@seg,'abc')}
+        Segmentator.publicize(:calculate_compression) do
+          seg = @segmentator.instance_eval{instance_variable_get(:@seg)}
+          @segmentator.should_receive(:add_to_legend)
+          @segmentator.calculate_compression(seg)
+        end
+      end
+    end
+
+    #asserts update_compression_order
+    context "both methods are called" do
+      it "should pass seg and segnum to add_to_legend and calculate_compression" do
+        #Check that both are asserted
         @segmentator.instance_eval{instance_variable_set(:@seg,'abc')}
         Segmentator.publicize(:calculate_compression) do
           seg = @segmentator.instance_eval{instance_variable_get(:@seg)}
@@ -178,7 +214,6 @@ describe Segmentator do
     #6. Data Flow: Defined-Used incrementing segvalue
     context "seg is not a legend key" do
       it "should add the seg to the legend and assign it a segvalue" do
-        @segmentator = Segmentator.new(3, "testcases/mango.txt")
         seg = 'abc'
         segval = @segmentator.instance_eval{instance_variable_get(:@segvalue)}
         Segmentator.publicize(:add_to_legend) do
@@ -197,7 +232,6 @@ describe Segmentator do
     #6. Data Flow: Defined-Used incrementing segvalue
     context "seg is already a legend key" do
       it "should leave the legend unchanged" do
-        @segmentator = Segmentator.new(3, "testcases/mango.txt")
         h = {'abc' => 1}
         legend = @segmentator.instance_eval{instance_variable_set(:@legend, h)}
         legend = @segmentator.instance_eval{instance_variable_get(:@legend)}
@@ -217,7 +251,6 @@ describe Segmentator do
     #4. Bad Data: seg is an object
     context "seg is not a string value" do
       it "should raise an error" do
-        @segmentator = Segmentator.new(3, "testcases/mango.txt")
         Segmentator.publicize(:add_to_legend) do
           lambda {@segmentator.add_to_legend(Object.new)}.should raise_error
         end
@@ -240,7 +273,6 @@ describe Segmentator do
     #7. Data Flow: Defined-Used compression order before/after values
     context "segnum is in the compression order" do
       it "moves segnum node to the front of the list and outputs the order" do
-        @segmentator = Segmentator.new(3, "testcases/mango.txt")
         legendvals = {'abc' => 1, 'def' => 2}
         @segmentator.instance_eval{instance_variable_set(:@legend, legendvals)}
         legend = @segmentator.instance_eval{instance_variable_get(:@legend)}
@@ -264,8 +296,6 @@ describe Segmentator do
     #7. Data Flow: Defined-Used compression order before/after values
     context "segnum is the only node to be added to the list" do
       it "adds it to the compression hash and moves to the front" do
-        @segmentator = Segmentator.new(3, "testcases/mango.txt")
-
         co = {1 => {"next" => nil, "prev" => nil}}
         @segmentator.instance_eval{instance_variable_set(:@compression_order, co)}
 
@@ -283,7 +313,6 @@ describe Segmentator do
     #7. Data Flow: Defined-Used compression order before/after values
     context "segnum is not in the compression order" do
       it "adds it to the compression hash and moves to the front" do
-        @segmentator = Segmentator.new(3, "testcases/mango.txt")
 
         co = {1 => {"next" => nil, "prev" => 2}, 2 => {"next" => 1, "prev" => nil}}
         @segmentator.instance_eval{instance_variable_set(:@compression_order, co)}
@@ -306,8 +335,6 @@ describe Segmentator do
     #7. Data Flow: Defined-Used compression order before/after values
     context "compression order is empty, this will be the first node" do
       it "adds a single node to the list" do
-        @segmentator = Segmentator.new(3, "testcases/mango.txt")
-
         co = {}
         @segmentator.instance_eval{instance_variable_set(:@compression_order, co)}
 
@@ -322,8 +349,6 @@ describe Segmentator do
     #7. Bad Data: segnum is an Object
     context "compression order is empty, this will be the first node" do
       it "adds a single node to the list" do
-        @segmentator = Segmentator.new(3, "testcases/mango.txt")
-
         co = {}
         @segmentator.instance_eval{instance_variable_set(:@compression_order, co)}
 
@@ -346,8 +371,6 @@ describe Segmentator do
     #1. Good Data: seglistposition is a number in the range of a length
     context "seglist position is a valid place in the list and has corresponding legend values" do
       it "should generate a line of the output table" do
-        @segmentator = Segmentator.new(3, "testcases/mango.txt")
-
         legendvals = {'abc' => 1, 'def' => 2, 'ghi' => 3}
         @segmentator.instance_eval{instance_variable_set(:@legend, legendvals)}
 
@@ -372,8 +395,6 @@ describe Segmentator do
     #1. Good Data: seglistposition is a number in the range of a length
     context "seglist position is a valid place in the list and has corresponding legend values" do
       it "should generate a line of the output table" do
-        @segmentator = Segmentator.new(3, "testcases/mango.txt")
-
         legendvals = {'abc' => 1, 'def' => 2, 'ghi' => 3}
         @segmentator.instance_eval{instance_variable_set(:@legend, legendvals)}
 
@@ -396,8 +417,6 @@ describe Segmentator do
     #Good Data: legend has at least one key-value pair
     context "legend has some valid values" do
       it "should output the formatted legend" do
-        @segmentator = Segmentator.new(3, "testcases/mango.txt")
-
         legendvals = {'abc' => 1, 'def' => 2, 'ghi' => 3}
         @segmentator.instance_eval{instance_variable_set(:@legend, legendvals)}
 
@@ -413,8 +432,6 @@ describe Segmentator do
     #Bad Data: legend is empty
     context "legend has some valid values" do
       it "should output the formatted legend" do
-        @segmentator = Segmentator.new(3, "testcases/mango.txt")
-
         legendvals = {}
         @segmentator.instance_eval{instance_variable_set(:@legend, legendvals)}
 
@@ -433,7 +450,6 @@ describe Segmentator do
     context "given a valid compression list" do
       it "should output the string representation of the list" do
         Segmentator.publicize(:list_string) do
-          @segmentator = Segmentator.new(3, "testcases/mango.txt")
           co = {1 => {"next" => nil, "prev" => 2}, 2 => {"next" => 1, "prev" => nil}}
           @segmentator.instance_eval{instance_variable_set(:@compression_order, co)}
           @segmentator.should_receive(:traverse_list)
@@ -446,7 +462,6 @@ describe Segmentator do
     context "given a valid compression list" do
       it "should output the string representation of the list" do
         Segmentator.publicize(:list_string) do
-          @segmentator = Segmentator.new(3, "testcases/mango.txt")
           co = {1 => {"next" => nil, "prev" => 2}, 2 => {"next" => 1, "prev" => nil}}
           @segmentator.instance_eval{instance_variable_set(:@compression_order, co)}
           @segmentator.list_string.should == "21"
@@ -458,7 +473,6 @@ describe Segmentator do
     context "given a compromised compression list" do
       it "should output the string representation of the list" do
         Segmentator.publicize(:list_string) do
-          @segmentator = Segmentator.new(3, "testcases/mango.txt")
           co = {1 => {"next" => nil, "prev" => nil}, 2 => {"next" => 1, "prev" => nil}}
           @segmentator.instance_eval{instance_variable_set(:@compression_order, co)}
           lambda{@segmentator.list_string}.should raise_error
@@ -472,7 +486,6 @@ describe Segmentator do
     context "given a valid compression list" do
       it "should assert traverse list" do
         Segmentator.publicize(:seg_list_position) do
-          @segmentator = Segmentator.new(3, "testcases/mango.txt")
           co = {1 => {"next" => nil, "prev" => 2}, 2 => {"next" => 1, "prev" => nil}}
           @segmentator.instance_eval{instance_variable_set(:@compression_order, co)}
           @segmentator.should_receive(:traverse_list)
@@ -485,7 +498,6 @@ describe Segmentator do
     context "given a valid compression list" do
       it "should return the order of the segment in the list" do
         Segmentator.publicize(:seg_list_position) do
-          @segmentator = Segmentator.new(3, "testcases/mango.txt")
           co = {1 => {"next" => nil, "prev" => 2}, 2 => {"next" => 1, "prev" => nil}}
           @segmentator.instance_eval{instance_variable_set(:@compression_order, co)}
           @segmentator.seg_list_position(1)
@@ -497,7 +509,6 @@ describe Segmentator do
     context "given an object instead of an integer" do
       it "should raise an error" do
         Segmentator.publicize(:seg_list_position) do
-          @segmentator = Segmentator.new(3, "testcases/mango.txt")
           co = {1 => {"next" => nil, "prev" => 2}, 2 => {"next" => 1, "prev" => nil}}
           @segmentator.instance_eval{instance_variable_set(:@compression_order, co)}
           lambda{@segmentator.seg_list_position(Object.new)}.should raise_error
@@ -513,7 +524,6 @@ describe Segmentator do
       it "loops through the entire list" do
         Segmentator.publicize(:traverse_list) do
           ct = 0
-          @segmentator = Segmentator.new(3, "testcases/mango.txt")
           co = {1 => {"next" => nil, "prev" => 2}, 2 => {"next" => 1, "prev" => nil}}
           @segmentator.instance_eval{instance_variable_set(:@compression_order, co)}
           @segmentator.traverse_list(nil){|nodenum| ct += 1}.should be 2
@@ -527,7 +537,6 @@ describe Segmentator do
       it "should never enter the list" do
         Segmentator.publicize(:traverse_list) do
           ct = 0
-          @segmentator = Segmentator.new(3, "testcases/mango.txt")
           co = {1 => {"next" => nil, "prev" => nil}}
           @segmentator.instance_eval{instance_variable_set(:@compression_order, co)}
           @segmentator.traverse_list(nil){|nodenum| ct += 1}.should be 1
@@ -540,7 +549,6 @@ describe Segmentator do
       it "should never enter the list" do
         Segmentator.publicize(:traverse_list) do
           ct = 0
-          @segmentator = Segmentator.new(3, "testcases/mango.txt")
           co = {}
           @segmentator.instance_eval{instance_variable_set(:@compression_order, co)}
           lambda{@segmentator.traverse_list(nil){|nodenum| ct += 1}}.should raise_error
